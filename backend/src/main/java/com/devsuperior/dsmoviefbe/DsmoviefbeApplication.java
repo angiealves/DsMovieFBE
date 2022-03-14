3753f9fe398e2c290fee14dd67a2e2bb0b2cb5a0
Angelo Alves <angelodaniel@hotmail.com>
2022-03-14 18:16:09 -0300

    Projeto criado

---
+++ backend/src/main/java/com/devsuperior/dsmoviefbe/DsmoviefbeApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmoviefbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmoviefbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmoviefbeApplication.class, args);
+	}
+
+}
